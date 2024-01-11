2d5990e87f2db8abe4c3865d3705e7f5c72c0292
deetz99 <dietrich@wolpert.ca>
2024-01-11 14:53:23 -0800

    tests not working

---
+++ tests/components/MyButton.spec.ts
@@ -0,0 +1,8 @@
+import { it, expect } from 'vitest'
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import MyButton from '~/components/MyButton.vue'
+
+it('can mount some component', async () => {
+  const component = await mountSuspended(MyButton)
+  expect(component.text()).toMatchInlineSnapshot('Hello World')
+})
---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath } from 'node:url'
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+
+export default defineVitestConfig({
+  test: {
+    dir: 'tests',
+    environment: 'nuxt',
+    // coverage: {
+    //   reportsDirectory: 'coverage',
+    // },
+    environmentOptions: {
+      nuxt: {
+        rootDir: fileURLToPath(new URL('./', import.meta.url)),
+        domEnvironment:
+          (process.env.VITEST_DOM_ENV as 'happy-dom' | 'jsdom') ?? 'happy-dom'
+        // mock: {
+        //   indexedDb: true,
+        // },
+      }
+    },
+    // setupFiles: './tests/setup/mocks.ts',
+    globals: true
+  }
+})
